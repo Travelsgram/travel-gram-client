030d7a54da71bb99c26a6c5e7ae419053014b315
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-13 12:32:04 +0200

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 
 import './App.css';
-<<<<<<< HEAD
-import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
-=======
 import { Routes, Route } from 'react-router-dom';
->>>>>>> dd97d3f16d949ca7f11f1b29e3257cebfdea53d5
 
 function App() {
   return (
@@ -15,11 +11,8 @@ function App() {
 
      <Navbar/>
       <Routes>
-<<<<<<< HEAD
       <Route path='/' element={<HomePage/>}/>
-=======
-        <Route path="/Register"/>
->>>>>>> dd97d3f16d949ca7f11f1b29e3257cebfdea53d5
+        <Route path="/Register" />
       </Routes>
     </div>
   );

78953c20fd373ec8803f232ed3e66a658a1e74c8
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-14 01:06:03 +0200

    travelguide details page is displayed

--- src/pages/TravelGuideDetails.js
+++ src/pages/TravelGuideDetails.js
@@ -1,39 +1,49 @@
-import { useParams } from "react-router";
-import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import { useEffect, useState, useContext } from "react"
+import { AuthContext } from "../context/auth.context";
 import axios from "axios";
 
-export default function TravelGuideDetails () {
-const [oneTravelGuide, setOneTravelGuide] = useState (null)
-    const { travelguideId } = useParams();
-    
-console.log(travelguideId);
+ function TravelGuideDetails () {
+  const [oneTravel, setOneTravel] = useState([]);
+  const { travelguideId } = useParams();
+  const { storedToken } = useContext(AuthContext);
 
-    useEffect(() => {
-        axios
-          .get(`${process.env.REACT_APP_API_URL}/api/travelguide/:${travelguideId}`)
-          .then((response) => {
-            setOneTravelGuide(response.data);
-          })
-          .catch((err) => console.log("error getting beers from API", err));
-      }, [travelguideId]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/api/travelguide/${travelguideId}`, { headers: { Authorization: `Bearer ${storedToken}` }})
+      .then((response) => {
+        if (response.data) {
+            console.log(response);
+          setOneTravel(response.data);
+          console.log(response.data);
+          console.log(response.data.activities);
+          console.log(oneTravel.activities);
+        } else {
+          console.log("No travel guide data found.");
+        }
+      })
+      .catch((err) => console.log("Error getting travel guide from API", err));
+  }, [travelguideId]); 
 
-const renderDetails = () => {
+  const renderDetails = () => {
+    if (!oneTravel) {
+      return <p>Loading details...</p>;
+    }
 
     return (
-        <>
-        <p> Activities : {oneTravelGuide.travel.activities} </p>
-        <p>Tips : {oneTravelGuide.travel.tips} </p>
-        <p>Location : {oneTravelGuide.travel.location}</p>
-        <p>Description: {oneTravelGuide.travel.description}</p>
-        </>
-            )
+      <>
+        <p>Activities: {oneTravel.activities}</p>
+        <p>Tips: {oneTravel.tips}</p>
+        <p>Location: {oneTravel.location}</p>
+        <p>Description: {oneTravel.description}</p>
+      </>
+    );
+  };
 
-
-}
-return (
+  return (
     <>
-       {oneTravelGuide ? renderDetails() :<p>Loading details...</p> }
+      {renderDetails()}
     </>
-   )
-    
+  );
 }
+export default TravelGuideDetails;

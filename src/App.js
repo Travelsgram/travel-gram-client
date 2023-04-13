dd97d3f16d949ca7f11f1b29e3257cebfdea53d5
Hristos Pappasgit config --global user.name Hristos <hristos.pappas@icloud.com>
2023-04-13 18:20:45 +0800

    added functionality to signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
 import './App.css';
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>hello</h1>
 
 
       <Routes>
-
+        <Route path="/Register"/>
       </Routes>
     </div>
   );

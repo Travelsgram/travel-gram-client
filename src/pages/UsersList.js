5d5cae29f07ef2995f0d6b1a3b44b9373ccfcb62
Hristos Pappasgit config --global user.name Hristos <hristos.pappas@icloud.com>
2023-04-16 11:09:11 +0800

    added follow function on user

--- src/pages/UsersList.js
+++ src/pages/UsersList.js
@@ -6,7 +6,7 @@ import { AuthContext } from "../context/auth.context";
 export default function UserProfile() {
 const [users, setUsers] = useState(null)
 
-const {storedToken} = useContext(AuthContext);
+const {storedToken, user} = useContext(AuthContext);
 
 useEffect(() => {
     axios
@@ -18,19 +18,35 @@ useEffect(() => {
       .catch((err) => console.log("error getting user from API", err));
   }, []);
 
+  const addFollow = (followId) => {
+
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/api/users/follow/${followId}`,
+        { headers: {Authorization: `Bearer ${storedToken}`}}
+        )
+      .then(response =>{
+        console.log(response);
+      })
+      .catch((err) => console.log("error getting user from API", err))
+
+  }
+
   return (
     <div>
       <h1>User Profile Information</h1>
 
       {users ? (
-        users.map((user) => {
+        users.map((thisUser) => {
           return (
-            <div key={user._id} className="User">
-            <p> Name: {user.name}</p>
-            <p> Email: {user.email}</p>
-            <img src={user.profileImg} alt={user.name} />
-            <p> Birthday : {user.birthdate} </p>
-            <p> Location: {user.location}</p>
+            <div key={thisUser._id} className="User">
+            <p> Name: {thisUser.name}</p>
+            <p> Email: {thisUser.email}</p>
+            <img src={thisUser.profileImg} alt={thisUser.name} />
+            <p> Birthday : {thisUser.birthdate} </p>
+            <p> Location: {thisUser.location}</p>
+
+            <button onClick={() => {addFollow(thisUser._id)}}>Follow</button>
     
             </div>
           );

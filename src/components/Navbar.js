62885ab9fcd399aa3e94de85911e869c88491b58
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-17 18:08:17 +0200

    profileImg displaying on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { AuthContext } from "../context/auth.context";
 import {Flex, Button, IconButton} from '@chakra-ui/react'
-import {HamburgerIcon, CloseIcon, Icon} from '@chakra-ui/icons'
-import { Text } from "@chakra-ui/layout";
+import {HamburgerIcon, CloseIcon} from '@chakra-ui/icons'
+import { Text } from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/avatar";
+
  
 function Navbar() {
   
@@ -14,11 +16,14 @@ function Navbar() {
     logOutUser         
   } = useContext(AuthContext);
 
-  const [display, changeDisplay] = useState('none')
+  const [display, changeDisplay] = useState('none');
+  
+
+ 
 
   return (
-    <Flex>
-     <Flex pos="fixed" top="1rem" right="1rem" align="center">
+    <Flex justify="center" alignItems="center">
+     <Flex pos="relative" top="1rem" right="1rem" align="center">
 
       <Flex display={['none','none' , 'flex' , 'flex']} >
 
@@ -65,10 +70,13 @@ function Navbar() {
           onClick={()=> changeDisplay('flex')}
          />
       
-      <Link to="/userprofile"> 
-      <Text>{user && user.name}</Text>
+      
+       <Flex spacing={3} alignItems="center" >
+       <Avatar name={user && user.name} src={user.profileImg}/>
+       <Link to="/userprofile"> 
+       <Text >{user && user.name}</Text>
       </Link>
-
+      </Flex>
      </Flex>
 
        <Flex w="100vw"
@@ -83,33 +91,40 @@ function Navbar() {
        display={display}>
 
        <Flex justify="flex-end">
-        <IconButton mt={2} mr={2} aria-label="Close Menu" size="lg" icon={<CloseIcon/>}  onClick={()=> changeDisplay('none')} />
+        <IconButton
+         mt={2}
+         mr={2} 
+         aria-label="Close Menu" 
+         size="lg"
+         icon={<CloseIcon/>} 
+        onClick={()=> changeDisplay('none')} />
        </Flex>
          
       <Flex flexDir="column" align="center">
       <Link to="/">
-        <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%">Home</Button>
+        <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%" onClick={() => changeDisplay('none')} >Home</Button>
       </Link>
 
       <Link to="/travelguide"> 
-      <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%"> Travel Guide</Button>
+      <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%" onClick={() => changeDisplay('none')} > Travel Guide</Button>
       </Link>
 
       <Link to="/users"> 
-      <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%"> User List </Button>
+      <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%" onClick={() => changeDisplay('none')} > User List </Button>
       </Link>
 
       {!isLoggedIn && 
       <>
         <Link to="/register"> 
-        <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%" > SignUp / Login</Button>
+        <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%" onClick={() => changeDisplay('none')} > SignUp / Login</Button>
         </Link>
         </>
       }
       
       {isLoggedIn &&
       <>
-        <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%" onClick={logOutUser}>Logout</Button>
+      
+        <Button as="a" variant="ghost" arial-label="Home" my={5} w="100%"  onClick={logOutUser} >Logout</Button>
         
       </>
         
